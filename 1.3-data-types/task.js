0e53871b9f2cbb784b37e42c8ea8d04fba606bba
Anastasia Shmeleva <shmeleva-nastya@mail.ru>
2021-03-02 19:37:31 +0000

    first commit data-types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,30 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    // percent = Number(percent);
+    // contribution = parseInt(contribution);
+    // amount = parseInt(percent);
+
+    // if (typeof(percent) !== 'number') {
+    //     return "Параметр \'Процентная ставка\' содержит неправильное значение" + " " + percent;
+    // }
+
+    let debitBody = amount - contribution; //тело кредита
+    let months = (date.getFullYear() - new Date().getFullYear()) * 12;  
+    let p = percent / 12 / 100; //1/12 процентной ставки (от 0 до 1)
+    let monthlyPayment = debitBody * (p + p / ((Math.pow(1 + p, months)) - 1)); 
+    let totalAmount = monthlyPayment * months;
+    totalAmount = +totalAmount.toFixed(2);
+
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (name === undefined || name === "") {
+        name = "Аноним";
+    } 
+
+    console.log(`Привет, мир! Меня зовут ${name}.`);
+    let greeting = `Привет, мир! Меня зовут ${name}.`;
+    return greeting;
 }
