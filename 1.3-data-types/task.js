8c804a350e1ef860bbc3cc442d748fe1ee4a888d
Anastasia Shmeleva <shmeleva-nastya@mail.ru>
2021-03-03 15:23:47 +0000

    second commit data-types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,17 +1,23 @@
 'use strict';
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // percent = Number(percent);
-    // contribution = parseInt(contribution);
-    // amount = parseInt(percent);
+    let percent2 = parseInt(percent);
+    let contribution2 = parseInt(contribution);
+    let amount2 = parseInt(amount);
 
-    // if (typeof(percent) !== 'number') {
-    //     return "Параметр \'Процентная ставка\' содержит неправильное значение" + " " + percent;
-    // }
+    if (Number.isNaN(percent2) || percent < 0) {
+        return `Параметр \'Процентная ставка\' содержит неправильное значение \'${percent}\'`;
+    } else if (Number.isNaN(contribution2) || contribution < 0) {
+        return `Параметр \'Начальный взнос\' содержит неправильное значение \'${contribution}\'`;
+    } else if (Number.isNaN(amount2) || amount < 0) {
+        return `Параметр \'Общая стоимость\' содержит неправильное значение \'${amount}\'`;
+    } else if (date < new Date() || date.getFullYear() == new Date().getFullYear()) {
+        return 'Параметр \'Срок ипотеки\' содержит неправильную дату';
+    }
 
-    let debitBody = amount - contribution; //тело кредита
+    let debitBody = amount2 - contribution2; //тело кредита
     let months = (date.getFullYear() - new Date().getFullYear()) * 12;  
-    let p = percent / 12 / 100; //1/12 процентной ставки (от 0 до 1)
+    let p = percent2 / 12 / 100; //1/12 процентной ставки (от 0 до 1)
     let monthlyPayment = debitBody * (p + p / ((Math.pow(1 + p, months)) - 1)); 
     let totalAmount = monthlyPayment * months;
     totalAmount = +totalAmount.toFixed(2);
